05ae614a9d738d8d8f37fac3b3f9de8692ecff27
Eugenio Hidalgo Hernàndez <eugenio.hidalgo@unit4.com>
2017-03-17 09:32:09 +0100

    Modified app structure to include a spec folder for testing.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var controllers = require ('./controllers');
-var data = require ('./data'); //<---- This is used to seed the initial data.
+var controllers = require ('./app/controllers');
+var data = require ('./app/data'); //<---- This is used to seed the initial data.
 
 //app.use(express.static(path.join(__dirname, 'api')));
 app.use(express.static(path.join(__dirname, '../client')));
